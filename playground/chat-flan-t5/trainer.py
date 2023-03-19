b75fb03d6837402daca1910175b0ab3a7a838bd9
Shauna Kravec <smkravec@chip.local>
2023-03-18 23:24:22 -0700

    cleanup and use alpaca

--- playground/chat-flan-t5/trainer.py
+++ playground/chat-flan-t5/trainer.py
@@ -1,30 +1,80 @@
 import torch
 import torch.nn.functional as F
 from datasets import load_dataset
+from torch.utils.data import DataLoader
 from accelerate import Accelerator
 import tqdm
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, default_data_collator, get_linear_schedule_with_warmup, DataCollatorForSeq2Seq
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    get_linear_schedule_with_warmup,
+    DataCollatorForSeq2Seq,
+)
+from peft import LoraConfig, TaskType, get_peft_model, get_peft_model_state_dict
 
+from playground.tracker.tracker import ConsoleTracker, Tracker
 
-from playground.tracker.tracker import Tracker
 
 class Trainer:
     def __init__(
         self,
         model_id: str = "google/flan-t5-xxl",
-        dataset_id: str = "Anthropic/hh-rlhf",
+        dataset_path: str = "./alpaca_data.json",
+        batch_size: int = 4,
+        max_length: int = 512,
         epochs: int = 1,
         lr: float = 1e-4,
+        tracker: Tracker = ConsoleTracker(),
     ):
-        self.accelerator = Accelerator()
+        ### Init
+
         self.epochs = epochs
         self.model_id = model_id
-        self.dataset_id = dataset_id
+        self.batch_size = batch_size
+        self.max_length = max_length
         self.tracker = tracker
+        self.checkpoint_name = "chaT5_lora.pt"
 
+        ### Load model
+
+        self.accelerator = Accelerator()
+
+        peft_config = LoraConfig(
+            task_type=TaskType.SEQ_2_SEQ_LM,
+            inference_mode=False,
+            r=8,
+            lora_alpha=32,
+            lora_dropout=0.1,
+        )
+        model = AutoModelForSeq2SeqLM.from_pretrained(self.model_id)
+        model = get_peft_model(model, peft_config)
+
+        ### Load dataset
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
-        model = AutoModelForSeq2SeqLM.from_pretrained(self.model_id).to(self.accelerator.device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        dataset = load_dataset(
+            "json",
+            data_files={
+                "train": dataset_path,
+            },
+            cache_dir="./cache",
+        )
+
+        with self.accelerator.main_process_first():
+            self.processed_dataset = dataset.map(
+                self.prepare_data, batched=True, batch_size=batch_size
+            )["train"]
+
+        data_collator = DataCollatorForSeq2Seq(self.tokenizer, model=model)
+
+        train_dataloader = DataLoader(
+            self.processed_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=data_collator,
+        )
+
+        ### Init optimizer
+        optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
         lr_scheduler = get_linear_schedule_with_warmup(
             optimizer=optimizer,
@@ -32,26 +82,61 @@ class Trainer:
             num_training_steps=(len(train_dataloader) * self.epochs),
         )
 
-        self.model, self.train_dataloader, self.eval_dataloader, self.optimizer, self.lr_scheduler = self.accelerator.prepare(
-            model, train_dataloader, eval_dataloader, optimizer, lr_scheduler
-        )
+        ### Prepare model
+
+        (
+            self.model,
+            self.train_dataloader,
+            self.optimizer,
+            self.lr_scheduler,
+        ) = self.accelerator.prepare(model, train_dataloader, optimizer, lr_scheduler)
         self.model = torch.compile(self.model)
-        self.accelerator.state.deepspeed_plugin.zero_stage == 3
 
-    
-    def prepare_data(self):
-        # Load dataset from the hub
-        dataset = load_dataset(self.dataset_id)
-        # Load tokenizer of FLAN-t5-base
+    @staticmethod
+    def format_prompt(instruction: str, input: str) -> str:
+        if input:
+            return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the requested task.
 
-        print(f"Train dataset size: {len(dataset['train'])}")
-        print(f"Test dataset size: {len(dataset['test'])}")
-    
+Instruction: {instruction}
+
+Input: {input}
+
+Response:"""
+        else:
+            return f"""Below is an instruction that describes a task. Write a response that appropriately completes the requested task.
+                
+Instruction: {instruction}
+
+Response:"""
+
+    def prepare_data(self, examples: list[dict[str, str]]):
+        inputs = [
+            self.format_prompt(example["instruction"], example["input"])
+            for example in examples
+        ]
+        model_inputs = self.tokenizer(
+            inputs,
+            max_length=self.max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+        with self.tokenizer.as_target_tokenizer():
+            labels = self.tokenizer(
+                [example["output"] for example in examples],
+                max_length=self.max_length,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt",
+            )
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+
     def train(self):
-
-        for epoch in range(self.epochs):
+        for _ in range(self.epochs):
             self.model.train()
             total_loss = 0
+            k=0
             for step, batch in enumerate(tqdm(self.train_dataloader)):
                 outputs = self.model(**batch)
                 loss = outputs.loss
@@ -60,35 +145,17 @@ class Trainer:
                 self.optimizer.step()
                 self.lr_scheduler.step()
                 self.optimizer.zero_grad()
-                if step%1000 == 0:
-                    print("loss: ",loss.detach().float())
+                self.tracker.log_iteration_time(batch.size(0), k)
+                if step % 1000 == 0:
+                    print("loss: ", loss.detach().float())
+                    self.tracker.add_scalar("metrics/train-loss", loss.detach().float(), k)
                     self.accelerator.wait_for_everyone()
                     if self.accelerator.is_main_process:
                         self.accelerator.save(
-                            get_peft_model_state_dict(self.model, state_dict=self.accelerator.get_state_dict(self.model)), checkpoint_name
+                            get_peft_model_state_dict(
+                                self.model,
+                                state_dict=self.accelerator.get_state_dict(self.model),
+                            ),
+                            self.checkpoint_name,
                         )
-            self.eval(epoch)
-    
-    def eval(self, epoch: int, total_loss: float) -> tuple[float, list[str]]:
-        self.model.eval()
-        eval_loss = 0
-        eval_preds = []
-        for _, batch in enumerate(tqdm(self.eval_dataloader)):
-            with torch.no_grad():
-                outputs = self.model(**batch)
-            loss = outputs.loss
-            eval_loss += loss.detach().float()
-            preds = self.accelerator.gather_for_metrics(torch.argmax(outputs.logits, -1)).detach().cpu().numpy()
-            eval_preds.extend(self.tokenizer.batch_decode(preds, skip_special_tokens=True))
-        eval_epoch_loss = eval_loss / len(self.train_dataloader)
-        eval_ppl = torch.exp(eval_epoch_loss)
-        train_epoch_loss = total_loss / len(self.eval_dataloader)
-        train_ppl = torch.exp(train_epoch_loss)
-        self.accelerator.print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
-
-        self.accelerator.wait_for_everyone()
-        self.accelerator.save(
-            get_peft_model_state_dict(self.model, state_dict=self.accelerator.get_state_dict(self.model)), checkpoint_name
-        )
-        self.accelerator.wait_for_everyone()
-        return eval_epoch_loss, eval_preds
+                k+=1
